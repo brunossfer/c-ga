648652e7cf753a982c89a08dbc94361f00996903
Paulo H. Souza <paulosouza.ti@gmail.com>
2017-06-21 15:20:35 -0300

    delete archives

--- GA-1.0 - function 2 Rosenbrock/Head/Config.h
+++
@@ -1,5 +0,0 @@
-#define MAXRANDOM 30                                    //  valor randomico maximo
-#define MAXCROMGENE 4                                   //  total de genes
-#define TOTALPOPULATION 600                               //  total de individuos
-#define CROSSOVER 25                                    //  probabilidade de cruzamento x%
-#define MUTATION 1                                     //  probabilidade de mutacao x%
