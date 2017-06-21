1af1f825eae3e1e35901e5a0911ed674e6e8386a
Bruno Simões <brunossfer@gmail.com>
2017-06-21 15:54:47 -0300

    Update Config.h

--- head/Config.h
+++ head/Config.h
@@ -1,5 +1,6 @@
-#define MAXRANDOM 30                                    //  valor randomico maximo
-#define MAXCROMGENE 4                                   //  total de genes
-#define TOTALPOPULATION 600                               //  total de individuos
-#define CROSSOVER 25                                    //  probabilidade de cruzamento x%
-#define MUTATION 1                                     //  probabilidade de mutacao x%
+#define MAXRANDOM 30                                  //  valor randomico maximo
+#define MAXCROMGENE 4                                 //  total de genes
+#define TOTALPOPULATION 600                           //  total de individuos
+#define CROSSOVER 25                                  //  probabilidade de cruzamento x%
+#define MUTATION 1                                    //  probabilidade de mutacao x%
+#define MAXGENERATION 20                                 //  numero de geracoes de individuos
