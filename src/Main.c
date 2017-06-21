7d40f4c39a9be1716ab7a50ec28e5212c3e2fdff
Bruno Simões <brunossfer@gmail.com>
2017-06-21 16:11:09 -0300

    Update Main.c

--- src/Main.c
+++ src/Main.c
@@ -14,11 +14,11 @@ int main ()
    while(search)                                             // enquanto nao encontrar o resultado
    {
       world -> generateProbability();                        // gera a probabilidade dos membros da populacao
-      world -> selectNextGeneration();                       //seleciona proxima geracao
-      world -> crossOver();                                  //faz cruzamento entre cromossomos
-      world -> mutation();                                   //faz mutacoes de genes na populacao
+      world -> selectNextGeneration();                       // seleciona proxima geracao
+      world -> crossOver();                                  // faz cruzamento entre cromossomos
+      world -> mutation();                                   // faz mutacoes de genes na populacao
       world -> generateEvaluation();                         // gera a avaliacao dos membros da populacao
-      search =  world -> findResult();                       //proucura pelo cromossomo que resolva a funcao caso encontre
+      search =  world -> findResult();                       // proucura pelo cromossomo que resolva a funcao caso encontre
       generation++;
    }
    printf("\nGeneration : %d \n\n", generation);
